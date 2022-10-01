09a8c2859149a08c64e3888ff497e13bf89ca660
Logon Aluno <logonrmlocal@fiap.com.br>
2022-09-30 22:23:31 -0300

    Fim exercício 2 | Fim do Checkpoint

---
+++ gerarSenha/js/senha.js
@@ -0,0 +1,18 @@
+const caracteres = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'U', 'V', 'W',
+'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 'u', 'v',
+'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '&', '*']
+
+function gerarSenha(caracteres) {
+    let senha = [];
+    for (i=0;i<20;i++) {
+        senha[i] = caracteres.splice(Math.floor(Math.random()*caracteres.length),1)
+    }
+    document.querySelector("#senhaGerada").textContent = senha.join('');
+}
+
+
+
+
+
+
+
